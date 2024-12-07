1c123d16086a2e6b9c794f78ba7718b5021d3e97
Ys <4900895+yschua@users.noreply.github.com>
2024-12-08 02:12:40 +1100

    Add day 5

---
+++ Day05_1.cs
@@ -0,0 +1,46 @@
+﻿class Day05_1
+{
+    static void Main(string[] args)
+    {
+        using var sr = new StreamReader(args[0]);
+        var graph = new Dictionary<int, List<int>>();
+        string line;
+        var result = 0;
+
+        while ((line = sr.ReadLine()) != "")
+        {
+            var nums = line.Split('|').Select(x => int.Parse(x)).ToList();
+            var key = nums[0];
+            var value = nums[1];
+            if (!graph.ContainsKey(key))
+            {
+                graph[key] = new();
+            }
+            graph[key].Add(value);
+        }
+
+        bool Check(List<int> pages)
+        {
+            foreach (var i in Enumerable.Range(0, pages.Count - 1))
+            {
+                var (prev, next) = (pages[i], pages[i + 1]);
+                if (!graph.ContainsKey(prev) || !graph[prev].Contains(next))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        while ((line = sr.ReadLine()) != null)
+        {
+            var pages = line.Split(',').Select(x => int.Parse(x)).ToList();
+            if (Check(pages))
+            {
+                result += pages[pages.Count / 2];
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
---
+++ Day05_2.cs
@@ -0,0 +1,59 @@
+﻿class Day05_2
+{
+    static void Main(string[] args)
+    {
+        using var sr = new StreamReader(args[0]);
+        var graph = new Dictionary<int, List<int>>();
+        string line;
+        var result = 0;
+
+        while ((line = sr.ReadLine()) != "")
+        {
+            var nums = line.Split('|').Select(x => int.Parse(x)).ToList();
+            var key = nums[0];
+            var value = nums[1];
+            if (!graph.ContainsKey(key))
+            {
+                graph[key] = new();
+            }
+            graph[key].Add(value);
+        }
+
+        bool Check(List<int> pages)
+        {
+            foreach (var i in Enumerable.Range(0, pages.Count - 1))
+            {
+                var prev = pages[i];
+                var next = pages[i + 1];
+                if (!graph.ContainsKey(prev) || !graph[prev].Contains(next))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        int GetMid(List<int> pages)
+        {
+            foreach (var page in pages)
+            {
+                if (graph.ContainsKey(page) && graph[page].Intersect(pages).Count() == pages.Count / 2)
+                {
+                    return page;
+                }
+            }
+            return 0;
+        }
+
+        while ((line = sr.ReadLine()) != null)
+        {
+            var pages = line.Split(',').Select(x => int.Parse(x)).ToList();
+            if (!Check(pages))
+            {
+                result += GetMid(pages);
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
