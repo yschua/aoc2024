9921428893d91a7bc6e420397ff27a834831d0fe
Ys <4900895+yschua@users.noreply.github.com>
2024-12-07 12:40:32 +1100

    Add day 1

---
+++ Day01_1.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+class Day01_1
+{
+    static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines(args[0]);
+        List<int> llist = new();
+        List<int> rlist = new();
+        foreach (var line in lines)
+        {
+            var m = Regex.Match(line, @"(\d+)\s+(\d+)");
+            llist.Add(int.Parse(m.Groups[1].Value));
+            rlist.Add(int.Parse(m.Groups[2].Value));
+        }
+        llist.Sort();
+        rlist.Sort();
+        var distance = 0;
+        foreach (var (l, r) in llist.Zip(rlist, (l, r) => (l, r)))
+        {
+            distance += Math.Abs(l - r);
+        }
+        Console.WriteLine(distance);
+    }
+}
---
+++ Day01_2.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+class Day01_2
+{
+    static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines(args[0]);
+        List<int> llist = new();
+        Dictionary<int, int> dict = new();
+        List<int> rlist = new();
+        foreach (var line in lines)
+        {
+            var m = Regex.Match(line, @"(\d+)\s+(\d+)");
+            llist.Add(int.Parse(m.Groups[1].Value));
+            var r = int.Parse(m.Groups[2].Value);
+            if (dict.ContainsKey(r))
+            {
+                dict[r]++;
+            }
+            else
+            {
+                dict.Add(r, 1);
+            }
+        }
+        var score = 0;
+        foreach (var l in llist)
+        {
+            if (dict.ContainsKey(l))
+            {
+                score += l * dict[l];
+            }
+        }
+        Console.WriteLine(score);
+    }
+}
