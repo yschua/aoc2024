4133c10f2b19b21f11f131c925d1900ff3c4236c
Ys <4900895+yschua@users.noreply.github.com>
2024-12-08 00:27:09 +1100

    Add day 4

---
+++ Day04_1.cs
@@ -0,0 +1,42 @@
+﻿class Day04_1
+{
+    static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines(args[0]);
+        var grid = lines.Select(line => line.ToCharArray()).ToArray();
+        var count = 0;
+
+        bool Match(int xStart, int yStart, int xOffset, int yOffset)
+        {
+            var str = "";
+            foreach (var i in Enumerable.Range(0, 4))
+            {
+                var x = xStart + xOffset * i;
+                var y = yStart + yOffset * i;
+                if (x < 0 || y < 0 || x >= grid[0].Length || y >= grid.Length)
+                {
+                    return false;
+                }
+                str += grid[y][x];
+            }
+            return str is "XMAS" or "SAMX";
+        }
+
+        for (var y = 0; y < grid.Length; y++)
+        {
+            for (var x = 0; x < grid[0].Length; x++)
+            {
+                var offsets = new[] { (1, 0), (0, 1), (1, 1), (-1, 1) };
+                foreach (var offset in offsets)
+                {
+                    if (Match(x, y, offset.Item1, offset.Item2))
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
---
+++ Day04_2.cs
@@ -0,0 +1,38 @@
+﻿class Day04_2
+{
+    static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines(args[0]);
+        var grid = lines.Select(line => line.ToCharArray()).ToArray();
+        var count = 0;
+
+        char Get(int x, int y)
+        {
+            if (x < 0 || y < 0 || x >= grid[0].Length || y >= grid.Length)
+            {
+                return '\0';
+            }
+            return grid[y][x];
+        }
+
+        for (var y = 0; y < grid.Length; y++)
+        {
+            for (var x = 0; x < grid[0].Length; x++)
+            {
+                var center = Get(x, y);
+                var topLeft = Get(x - 1, y - 1);
+                var topRight = Get(x + 1, y - 1);
+                var botLeft = Get(x - 1, y + 1);
+                var botRight = Get(x + 1, y + 1);
+                var diag1 = $"{topLeft}{center}{botRight}";
+                var diag2 = $"{botLeft}{center}{topRight}";
+                if (diag1 is "SAM" or "MAS" && diag2 is "SAM" or "MAS")
+                {
+                    count++;
+                }
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
