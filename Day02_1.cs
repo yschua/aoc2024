3dea4e8db643ec83564fd0afbc9696b5e80853a8
Ys <4900895+yschua@users.noreply.github.com>
2024-12-07 14:09:01 +1100

    Add day 2

---
+++ Day02_1.cs
@@ -0,0 +1,37 @@
+﻿class Day02_1
+{
+    static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines(args[0]);
+        var safe = 0;
+        foreach (var line in lines)
+        {
+            var levels = line.Split(' ').Select(x => int.Parse(x)).ToList();
+            bool Check()
+            {
+                bool increasing = true;
+                bool decreasing = true;
+                for (var i = 0; i < levels.Count - 1; i++)
+                {
+                    var delta = levels[i + 1] - levels[i];
+                    increasing = increasing && delta > 0;
+                    decreasing = decreasing && delta < 0;
+                    if (!increasing && !decreasing)
+                    {
+                        return false;
+                    }
+                    if (Math.Abs(delta) > 3)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            if (Check())
+            {
+                safe++;
+            }
+        }
+        Console.WriteLine(safe);
+    }
+}
---
+++ Day02_2.cs
@@ -0,0 +1,56 @@
+﻿class Day02_2
+{
+    static void Main(string[] args)
+    {
+        bool CheckInner(List<int> levels)
+        {
+            bool increasing = true;
+            bool decreasing = true;
+            for (var i = 0; i < levels.Count - 1; i++)
+            {
+                var delta = levels[i + 1] - levels[i];
+                increasing = increasing && delta > 0;
+                decreasing = decreasing && delta < 0;
+                if (!increasing && !decreasing)
+                {
+                    return false;
+                }
+                if (Math.Abs(delta) > 3)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        bool CheckOuter(List<int> levels)
+        {
+            if (CheckInner(levels))
+            {
+                return true;
+            }
+            for (var i = 0; i < levels.Count; i++)
+            {
+                var copy = levels.ToList();
+                copy.RemoveAt(i);
+                if (CheckInner(copy))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        var lines = File.ReadAllLines(args[0]);
+        var count = 0;
+        foreach (var line in lines)
+        {
+            var levels = line.Split(' ').Select(x => int.Parse(x)).ToList();
+            if (CheckOuter(levels))
+            {
+                count++;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
