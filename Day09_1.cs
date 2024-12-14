eb26820ce653017f48babeb936b69f6f0c3478d9
Ys <4900895+yschua@users.noreply.github.com>
2024-12-15 00:33:58 +1100

    Add day 9

---
+++ Day09_1.cs
@@ -0,0 +1,58 @@
+﻿class Day09_1
+{
+    static void Main(string[] args)
+    {
+        var line = File.ReadAllLines(args[0])[0];
+        var blocks = new List<int>();
+        foreach (var i in Enumerable.Range(0, line.Length))
+        {
+            var value = (i % 2 == 0) ? i / 2 : -1;
+            var count = line[i] - '0';
+            blocks.AddRange(Enumerable.Repeat(value, count));
+        }
+
+        int FindFreeRight(int idx)
+        {
+            while (blocks[idx] != -1)
+            {
+                idx++;
+            }
+            return idx;
+        }
+
+        int FindFileLeft(int idx)
+        {
+            while (blocks[idx] == -1)
+            {
+                idx--;
+            }
+            return idx;
+        }
+
+        var leftIdx = 0;
+        var rightIdx = blocks.Count - 1;
+        while (true)
+        {
+            leftIdx = FindFreeRight(leftIdx);
+            rightIdx = FindFileLeft(rightIdx);
+            if (leftIdx >= rightIdx)
+            {
+                break;
+            }
+            blocks[leftIdx] = blocks[rightIdx];
+            blocks[rightIdx] = -1;
+            leftIdx++;
+            rightIdx--;
+        }
+
+        long checksum = 0;
+        foreach (var i in Enumerable.Range(0, blocks.Count))
+        {
+            if (blocks[i] != -1)
+            {
+                checksum += i * blocks[i];
+            }
+        }
+        Console.WriteLine(checksum);
+    }
+}
---
+++ Day09_2.cs
@@ -0,0 +1,94 @@
+﻿class Day09_2
+{
+    record struct Block(int Id, int Size);
+
+    static void Main(string[] args)
+    {
+        var line = File.ReadAllLines(args[0])[0];
+        var blocks = new LinkedList<Block>();
+        foreach (var i in Enumerable.Range(0, line.Length))
+        {
+            var id = (i % 2 == 0) ? i / 2 : -1;
+            var size = line[i] - '0';
+            if (size > 0)
+            {
+                blocks.AddLast(new Block(id, size));
+            }
+        }
+
+        LinkedListNode<Block> FindFile(int id)
+        {
+            for (var node = blocks.Last; node != null; node = node.Previous)
+            {
+                if (node.Value.Id == id)
+                {
+                    return node;
+                }
+            }
+            throw new InvalidOperationException();
+        }
+
+        LinkedListNode<Block>? FindFree(LinkedListNode<Block> nodeToMove)
+        {
+            for (var node = blocks.First; node != nodeToMove; node = node.Next)
+            {
+                if (node.Value.Id == -1 && node.Value.Size >= nodeToMove.Value.Size)
+                {
+                    return node;
+                }
+            }
+            return null;
+        }
+
+        void MergeFreeNodes(LinkedListNode<Block> a, LinkedListNode<Block> b)
+        {
+            if (a == null || b == null || a.Value.Id != -1 || b.Value.Id != -1)
+            {
+                return;
+            }
+            if (a.Next != b)
+            {
+                throw new InvalidOperationException();
+            }
+            a.ValueRef.Size += b.Value.Size;
+            blocks.Remove(b);
+        }
+
+        foreach (var id in Enumerable.Range(0, blocks.Last().Id + 1).Reverse())
+        {
+            var nodeToMove = FindFile(id);
+            var nodeToFill = FindFree(nodeToMove);
+            if (nodeToFill == null)
+            {
+                continue;
+            }
+
+            var freeSize = nodeToFill.Value.Size;
+            nodeToFill.Value = nodeToMove.Value;
+            var padding = freeSize - nodeToMove.Value.Size;
+            if (padding > 0)
+            {
+                blocks.AddAfter(nodeToFill, new Block(-1, padding));
+            }
+
+            nodeToMove.ValueRef.Id = -1;
+            MergeFreeNodes(nodeToMove, nodeToMove.Next);
+            MergeFreeNodes(nodeToMove.Previous, nodeToMove);
+        }
+
+        long checksum = 0;
+        int position = 0;
+        foreach (var block in blocks)
+        {
+            foreach (var _ in Enumerable.Range(0, block.Size))
+            {
+                if (block.Id != -1)
+                {
+                    checksum += position * block.Id;
+                }
+                position++;
+            }
+        }
+        Console.WriteLine(checksum);
+    }
+}
