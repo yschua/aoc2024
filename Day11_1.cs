59bfe735594c8442704b4524239fbf3f77a9bc43
Ys <4900895+yschua@users.noreply.github.com>
2024-12-18 02:46:29 +1100

    Add day 11

---
+++ Day11_1.cs
@@ -0,0 +1,39 @@
+﻿class Day11_1
+{
+    static void Main(string[] args)
+    {
+        var stones = File.ReadAllLines(args[0])[0].Split(' ').ToList();
+
+        List<string> Blink(List<string> stones)
+        {
+            var blinked = new List<string>();
+            foreach (var stone in stones)
+            {
+                if (stone == "0")
+                {
+                    blinked.Add("1");
+                }
+                else if (stone.Length % 2 == 0)
+                {
+                    var mid = stone.Length / 2;
+                    var left = stone.Substring(0, mid);
+                    var right = long.Parse(stone.Substring(mid, mid)).ToString();
+                    blinked.Add(left);
+                    blinked.Add(right);
+                }
+                else
+                {
+                    blinked.Add($"{long.Parse(stone) * 2024}");
+                }
+            }
+            return blinked;
+        }
+
+        foreach (var _ in Enumerable.Range(0, 25))
+        {
+            stones = Blink(stones);
+        }
+
+        Console.WriteLine(stones.Count);
+    }
+}
---
+++ Day11_2.cs
@@ -0,0 +1,53 @@
+﻿class Day11_2
+{
+    record struct Key(string Stone, int Blinks);
+
+    static void Main(string[] args)
+    {
+        var stones = File.ReadAllLines(args[0])[0].Split(' ').ToList();
+        var dp = new Dictionary<Key, long>();
+
+        long Blink(Key key)
+        {
+            if (dp.ContainsKey(key))
+            {
+                return dp[key];
+            }
+
+            var stone = key.Stone;
+            var blinks = key.Blinks;
+            long count = 0;
+
+            if (blinks == 1)
+            {
+                count = (stone.Length % 2 == 0) ? 2 : 1;
+            }
+            else if (stone == "0")
+            {
+                count = Blink(new("1", blinks - 1));
+            }
+            else if (stone.Length % 2 == 0)
+            {
+                var mid = stone.Length / 2;
+                var left = stone.Substring(0, mid);
+                var right = long.Parse(stone.Substring(mid, mid)).ToString();
+                count = Blink(new (left, blinks - 1)) + Blink(new(right, blinks - 1));
+            }
+            else
+            {
+                var multipled = $"{long.Parse(stone) * 2024}";
+                count = Blink(new(multipled, blinks - 1));
+            }
+
+            dp[key] = count;
+            return count;
+        }
+
+        long count = 0;
+        foreach (var stone in stones)
+        {
+            count += Blink(new(stone, 75));
+        }
+        Console.WriteLine(count);
+    }
+}
