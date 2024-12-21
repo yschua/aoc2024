28b051ba348c32e1fcfdedb2e10b8aff2d9b802a
Ys <4900895+yschua@users.noreply.github.com>
2024-12-22 00:42:13 +1100

    Add day 13

---
+++ Day13_2.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+class Day13_2
+{
+    static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines(args[0]);
+        var total = 0.0;
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (lines[i] == "")
+            {
+                continue;
+            }
+
+            var m_a = Regex.Match(lines[i], @"Button A: X\+(\d+), Y\+(\d+)");
+            var x_a = int.Parse(m_a.Groups[1].Value);
+            var y_a = int.Parse(m_a.Groups[2].Value);
+
+            var m_b = Regex.Match(lines[i + 1], @"Button B: X\+(\d+), Y\+(\d+)");
+            var x_b = int.Parse(m_b.Groups[1].Value);
+            var y_b = int.Parse(m_b.Groups[2].Value);
+
+            var m_p = Regex.Match(lines[i + 2], @"Prize: X=(\d+), Y=(\d+)");
+            var x_p = long.Parse(m_p.Groups[1].Value) + 10000000000000;
+            var y_p = long.Parse(m_p.Groups[2].Value) + 10000000000000;
+
+            var a = 1.0 * (x_b * y_p - x_p * y_b) / (x_b * y_a - x_a * y_b);
+            var b = 1.0 * (x_p - a * x_a) / x_b;
+
+            if (a == (long)a && b == (long)b)
+            {
+                total += 3 * a + b;
+            }
+
+            i = i + 2;
+        }
+
+        Console.WriteLine((long)total);
+    }
+}
